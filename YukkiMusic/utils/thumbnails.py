0429a620aac3abad5b8001605772baf3a24ea3f4
Chiranjib koch <93111779+Chiranjibkoch@users.noreply.github.com>
2023-07-28 21:59:57 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -123,7 +123,7 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2) 
-            image4.text((960, 10), "AAROHI MEDIA PLAYER", fill="white", font = font1, align ="left")
+            image4.text((960, 10), "AAROHI", fill="white", font = font1, align ="left")
             image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
@@ -136,9 +136,9 @@ async def gen_thumb(videoid):
             duration = f"Duration : {duration} Mins"
             channel = f"Channel : {channel}"
 
-            image4.text((670, 450), text=views, fill="red", font = font4, align ="left") 
+            image4.text((670, 450), text=views, fill="white", font = font4, align ="left") 
             image4.text((670, 500), text=duration, fill="white", font = font4, align ="left") 
-            image4.text((670, 550), text=channel, fill="green", font = font4, align ="left")
+            image4.text((670, 550), text=channel, fill="white", font = font4, align ="left")
             
             image2 = ImageOps.expand(image2,border=20,fill=make_col())
             image2 = image2.convert('RGB')
