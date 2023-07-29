efd7de8da7aac6779977c2b5ee1b1ad38a368dd0
Chiranjib koch <93111779+Chiranjibkoch@users.noreply.github.com>
2023-07-29 15:56:35 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -123,7 +123,7 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2) 
-            image4.text((960, 10), "AAROHI", fill="white", font = font1, align ="left")
+            image4.text((960, 10), "TRISHA", fill="white", font = font1, align ="left")
             image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
